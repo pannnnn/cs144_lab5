a83969e65f5b0f0554d3755573202453871cb485
Zhang Wen Hao <zhan1782@cdf.toronto.edu>
2016-12-02 21:20:47 -0500

    hhfgh

--- router/sr_router.c
+++ router/sr_router.c
@@ -747,7 +747,7 @@ void nat_handle_ip(struct sr_instance* sr,
               sr_handle_ip(sr, packet, len, ETH2);
               free(lookup_ext);                                                 
             }
-          } else if ((ntohs(tcp_packet->flags) & 0x10) >> 4){
+          } /*else if ((ntohs(tcp_packet->flags) & 0x10) >> 4){
             if (lookup_ext) {
               pthread_mutex_lock(&((sr->nat)->lock));
               struct sr_nat_mapping *ext_mapping = sr_nat_external_mapping(sr->nat,
@@ -762,7 +762,7 @@ void nat_handle_ip(struct sr_instance* sr,
               pthread_mutex_unlock(&((sr->nat)->lock));
               tcp_packet->dst_port = lookup_ext->aux_int;
               ip_packet->ip_dst = lookup_ext->ip_int;
-              /*tcp_checksum(packet, len, sr);*/
+
               tcp_packet->checksum = 0;
               sr_tcp_pseudo_hdr_t* tcp_pseudo_hdr = malloc(sizeof(sr_tcp_pseudo_hdr_t));
 
@@ -781,14 +781,14 @@ void nat_handle_ip(struct sr_instance* sr,
               free(tcp_pseudo_hdr);
               /*free(ptr);*/
               /*tcp_packet->checksum = cksum(tcp_packet, htons(ip_packet->ip_len)-ip_packet->ip_hl*4);*/
-              ip_packet->ip_sum = 0;
+              /*ip_packet->ip_sum = 0;
               ip_packet->ip_sum = cksum(ip_packet, ip_packet->ip_hl*4);
               print_hdr_ip(ip_packet);
               printf("TTTTTTTTTTRing to sent the packet for incoming tcp packet !!!\n");
               sr_handle_ip(sr, packet, len, ETH2);
               free(lookup_ext);             
             }
-          } else if (!lookup_ext) {
+          } */else if (!lookup_ext) {
             return;
           } else if (ntohs(tcp_packet->flags) & 0x1) {
             pthread_mutex_lock(&((sr->nat)->lock));
